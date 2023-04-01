8cb5e5a50550eb47f435e7440f2b49efb239643d
PRIYANSHU SUTAR <21BCS2797@cuchd.in>
2023-04-01 22:37:01 +0530

    third commit

---
+++ Multiply.java
@@ -0,0 +1,14 @@
+// Java program to illustrate the
+// Java.lang.Integer.sum() method
+import java.lang.*;
+  
+public class Multiply {
+  
+    public static void main(String[] args)
+    {
+        int a = 62;
+        int b = 18;
+        // It will return the sum of two arguments.
+        System.out.println(a-b);
+    }
+}
